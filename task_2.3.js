2b99ac90aa8932a438e4e2bb3e73230eacb51dd8
kaleem99 <kaleem1999@outlook.com>
2021-03-31 19:42:01 +0200

    fixed error

--- task_2.3.js
+++ task_2.3.js
@@ -2,6 +2,12 @@ function columns(arr) {
     let res = "";
     let newArr = [...arr];
     let largest = newArr.sort((a, b) => (b.length - a.length))[0];
+    console.log(largest.length)
+    for(let k = 0; k < arr.length; k++){
+        if(arr[k].length == largest.length){
+            largest = arr[k]
+        }
+    }
     let difference = Math.abs(arr.length - largest.length);
     for(let index = 0; index < difference; index++){
         arr.push("");
@@ -20,7 +26,8 @@ function columns(arr) {
         res += "\n";
     }
     let lastIndex = res.lastIndexOf(largest[largest.length-1]);
+    // console.log(largest)
     console.log(res.slice(0, lastIndex+1));
 }
 
-columns(["Write","good","coderbyte", "every","day", "long", "johnnyStorm"]);
+columns(["Write","good","code", "every","day"]);
